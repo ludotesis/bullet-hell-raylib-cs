151c9f6644b9f3133ed62d9b3ac1dfaa104a94cd
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-05-26 10:48:16 -0300

    Main Loop Templaye

--- source/Program.cs
+++ source/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Importar Raylib 
+using Raylib_cs;
+// Inicializar ventana
+Raylib.InitWindow(320, 180, "Bullet Hell con Raylib");
+// Verificar si la aplicación debe cerrarse (se presionó la tecla ESC o se hizo clic en el ícono de cerrar ventana)
+while (!Raylib.WindowShouldClose())
+{
+    // Establecer el color de fondo
+    Raylib.ClearBackground(Color.Beige);
+    // Configurar el canvas (framebuffer) para comenzar a dibujar
+    Raylib.BeginDrawing();
+    // Dibujar un texto en pantalla [posicionX,posicionY, size, color]
+    Raylib.DrawText("HOLA MAIN LOOP!", 50, 100,75, Color.Brown);
+    // Finalizar el dibujo del Canvas 
+    Raylib.EndDrawing();
+}
+// Cerrar ventana
+Raylib.CloseWindow();
