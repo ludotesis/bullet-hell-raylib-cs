5c6017338e56105da9a224c78cfac659887f213d
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-05-26 18:18:25 -0300

    Activo Shuriken

--- source/Program.cs
+++ source/Program.cs
@@ -5,6 +5,7 @@ using System.Numerics;
 Vector2 positionShuriken = new Vector2(100, 400);
 float velocidadShuriken = 400f;
 Texture2D spriteShuriken;
+bool activoShuriken = false;
 // Inicializar ventana
 Raylib.InitWindow(320, 180, "Bullet Hell con Raylib");
 //Configurar ventana sin border
@@ -25,15 +26,24 @@ while (!Raylib.WindowShouldClose())
     Raylib.ClearBackground(Color.Beige);
     // Dibujar un texto en pantalla [posicion en X,posicion en Y, tamaño, color]
     Raylib.DrawText("SUSCRIBETE A LUDOTESIS >.<", 50, 100, 75, Color.Brown);
-    // Mover Shuriken horizontalmente en Sincronización delta
-    positionShuriken.X += velocidadShuriken * deltaTime;
-    // Reiniciar Posicion Shuriken si supera limite horizontal
-    if (positionShuriken.X > Raylib.GetScreenWidth())
+    if (Raylib.IsKeyPressed(KeyboardKey.F))
     {
-        positionShuriken.X = 100;
+        activoShuriken = true;
     }
-    // Dibujar Textura en pantalla [textura2D, vector2, color]
-    Raylib.DrawTextureV(spriteShuriken, positionShuriken, Color.White);
+
+    if (activoShuriken)
+    {
+        // Mover Shuriken horizontalmente en Sincronización delta
+        positionShuriken.X += velocidadShuriken * deltaTime;
+        // Reiniciar Posicion Shuriken si supera limite horizontal
+        if (positionShuriken.X > Raylib.GetScreenWidth())
+        {
+            positionShuriken.X = 100;
+        }
+        // Dibujar Textura en pantalla [textura2D, vector2, color]
+        Raylib.DrawTextureV(spriteShuriken, positionShuriken, Color.White);
+    }
+  
     // Finalizar el dibujo del Canvas 
     Raylib.EndDrawing();
 }
