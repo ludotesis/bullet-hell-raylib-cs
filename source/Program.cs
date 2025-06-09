2f359eb97fe3fb1b182d3f6f273e9ce522d7faf0
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-09 10:20:28 -0300

    Refactor: ActualizarHitboxes, MoverShuriken, DispararShuriken, DispararEnemigo y  AnimarEnemigo

--- source/Program.cs
+++ source/Program.cs
@@ -21,7 +21,7 @@ bool activoShuriken = false;
 Rectangle hitboxShuriken;
 // =========== Enemigo ===== //
 Vector2 posicionEnemigo ;
-float velocidadEnemigo = 400f;
+float velocidadEnemigo = 150f;
 int dificultad = 3;
 velocidadEnemigo = velocidadEnemigo * dificultad;
 Texture2D spriteEnemigo;
@@ -30,7 +30,7 @@ bool upEnemigo = true;
 Rectangle hitboxEnemigo;
 // =========== Animacion Enemigo Reposo ===== //
 float duracionAnimacion = 1.0f;
-float velocidadAnimacion = 6f;
+float velocidadAnimacion = 5f;
 int cantidadFrames = 4;
 float tiempoActualAnimacion = 0;
 int frameActualAnimacion = 0;
@@ -38,10 +38,10 @@ Texture2D enemigoAnimamacionReposo;
 Rectangle RectanguloAnimacion;
 // =========== Enemigo Disparo ===== //
 Vector2 posicionDisparo;
-float velocidadDisparo = 700f;
-bool activoDisparo = true;
+float velocidadDisparo = 800f;
+bool activoDisparo = false;
 float radioDisparo = 18f;
-float refrescoDisparo = 3f;
+float refrescoDisparo = 4f;
 float tiempoEsperaDisparo = 0;
 // =========== Pared ===== //
 Vector2 posicionPared;
@@ -51,15 +51,12 @@ int anchoPared = 10;
 bool collisionShurikenMapa = false;
 bool collisionShurikenEnemigo = false;
 bool collisionDisparoJugador = false;
-#endregion
-
+// =========== Otros ===== //
 bool isDebugMode = false;
 float deltaTime = 0f;
-
+#endregion
 #region ==== CARGA DE JUEGO ====
 Raylib.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bullet Hell con Raylib");
-//Configurar ventana sin border
-//Raylib.ToggleBorderlessWindowed();
 // =========== Cargar Texturas ===== //
 CargarTexturas();
 // =========== Redimensionar Texturas ===== //
@@ -95,161 +92,29 @@ float limiteDown = Raylib.GetScreenHeight() - spriteJugador.Height;
 float origenShurikenX = posicionJugador.X + spriteJugador.Width;
 // Hasta que la aplicacion no se cierre
 float tiempoEntreFrame = duracionAnimacion / velocidadAnimacion;
-int columnaAnimacionY = 0;
+int columnaAnimacionY = 2;
 #endregion
 #region ==== GAME LOOP ====
 while (!Raylib.WindowShouldClose())
 {
-    // Obtener el tiempo en segundos para el último fotograma dibujado (tiempo delta)
-    deltaTime = Raylib.GetFrameTime();
-    // Sumamos tiempo trascurrido al disparo
-    tiempoEsperaDisparo += deltaTime;
-    // Se acumula tiempo para determinar cuando cambiar el frame
-    tiempoActualAnimacion += deltaTime;
-    // Si hay que cambiar de cuandro de animacion (segun duracion y velocidad)
-    //velocidadAnimacion++;
-    if (tiempoActualAnimacion >= tiempoEntreFrame)
-    {
-        //Reiniciar el conteo para el siguiente cuadro
-        tiempoActualAnimacion = 0f;
-        //Incrementar el INDICE del frame
-        frameActualAnimacion++;
-        //Si se supera el último frame
-
-        if (frameActualAnimacion >= cantidadFrames)
-        {
-            //Reiniciar la animacion
-            frameActualAnimacion = 0;
-        }
-        RectanguloAnimacion.X = RectanguloAnimacion.Width * columnaAnimacionY;
-        //Posicionar el rectángulo en el siguiente cuadro
-        RectanguloAnimacion.Y = frameActualAnimacion * RectanguloAnimacion.Height;
-    }
-   
-    // Establecer mensaje para ver frame actual del enemigo
-    Raylib.DrawText("FRAME ENEMIGO " + frameActualAnimacion.ToString(), 10, 1000, 30, Color.DarkBrown);
-    // Si el jugador presiona la tecla UP y no supera el limite superior
+    ActualizarTemporizadores();
+    //========= MOVIMIENTO =========
     MoverJugador();
+    MoverEnemigo();
+    // ======== DISPAROS ===========
     DispararShuriken();
-    // Si el Shuriken esta activo
-    if (activoShuriken)
-    {
-        // Mover Shuriken horizontalmente en Sincronización delta
-        posicionShuriken.X += velocidadShuriken * deltaTime;
-        // Actualizar Posición Hitbox segun posicion Disparo
-       
-        // Detectamos colision entre hibtox Shuriken y hibox pared luego de moverlo
-        collisionShurikenMapa = Raylib.CheckCollisionRecs(hitboxShuriken, hitboxPared);
-        // Reiniciar Posicion Shuriken si toca pared
-        if (collisionShurikenMapa)
-        {
-            activoShuriken = false;
-            posicionShuriken.X = origenShurikenX;
-        }
-        // Dibujar Textura en pantalla [textura2D, vector2, color]
-
-        // Dibujar Hibox en pantalla [Rectangulo, color] (Modo Debug)
-
-    }
-    // Si el Jugador esta activo
-    if (activoJugador)
-    {
-        
-        // Dibujar Textura en pantalla [textura2D, vector2, color]
-     
-        // Dibujar Hibox en pantalla [Rectangulo, color] (Modo Debug)
-
-    }
-    else
-    {
-        // Dibujar un texto en pantalla [posicion en X,posicion en Y, tamaño, color]
-        Raylib.DrawText("GAME OVER", 50, 100, 75, Color.Red);
-    }
-
-    // Si el Enemigo esta activo
-    if (activoEnemigo)
-    {
-        // Si el enemigo se mueve hacia arriba y no supera el limite superior 
-        if (upEnemigo && (posicionEnemigo.Y > limiteUp))
-        {
-            posicionEnemigo.Y -= velocidadEnemigo * deltaTime;
-        }
-        else
-        {
-            upEnemigo = false;
-        }
-        // Si el enemigo se mueve hacia abajo y no supera el limite inferior
-        if (!upEnemigo && (posicionEnemigo.Y < limiteDown))
-        {
-            posicionEnemigo.Y += velocidadEnemigo * deltaTime;
-        }
-        else
-        {
-            upEnemigo = true;
-        }
-        // Actualizar Posición Hitbox segun posicion Enemigo
-       
-        // Detectamos colision entre hibtox Shuriken y hibox Enemigo 
-        collisionShurikenEnemigo = Raylib.CheckCollisionRecs(hitboxShuriken, hitboxEnemigo);
-        // Reiniciar Posicion Shuriken si toca pared
-        if (collisionShurikenEnemigo)
-        {
-            activoShuriken = false;
-            posicionShuriken.X = origenShurikenX;
-            activoEnemigo = false;
-        }
-        // Dibujar una porción de la textura usando un rectángulo
-        //Raylib.DrawTextureV(spriteEnemigo, posicionEnemigo, Color.DarkBlue);
-        
-    
-    }
-    else
-    {
-        // Dibujar un texto en pantalla [posicion en X,posicion en Y, tamaño, color]
-        Raylib.DrawText("VICTORIA", 50, 100, 75, Color.DarkGreen);
-    }
-    // Si tiempo espera disparo es mayo o igual que el refresco de disparo
-    if ((tiempoEsperaDisparo >= refrescoDisparo) && activoEnemigo)
-    {
-        // Configuro el disparo como activado
-        activoDisparo = true;
-        // Reinicio el tiempo de espera para el siguiente disparo
-        tiempoEsperaDisparo = 0;
-        // Posicion Disparo en X segun Posicion Enemigo
-        posicionDisparo.X = posicionEnemigo.X;
-        // Posicion Disparo en Y segun Posicion Enemigo
-        posicionDisparo.Y = posicionEnemigo.Y + (spriteEnemigo.Height * 0.4f);
-        //
-        columnaAnimacionY = 2;
-    }
-    // Si el Disparo esta activo
-    if (activoDisparo)
-    {
-        // Mover Disparo horizontalmente en Sincronización delta
-        posicionDisparo.X -= velocidadDisparo * deltaTime;
-        // Detectamos colision entre hibtox Disparo y hibox Jugador
-        collisionDisparoJugador = Raylib.CheckCollisionCircleRec(posicionDisparo, radioDisparo, hitboxJugador);
-        // Si colision Disparo Jugador
-        if (collisionDisparoJugador)
-        {
-            activoJugador = false;
-            activoDisparo = false;
-            tiempoEsperaDisparo = 0;
-        }
-       
-    }
-    // Si el Disparo supera limite izquierdo
-    if (posicionDisparo.X < 0)
-    {
-        activoDisparo = false;
-        posicionDisparo.X = posicionEnemigo.X;
-    }
-    //========= INTERACTUAR =========
+    DispararEnemigo();
+    MoverShuriken();
+    MoverDisparoEnemigo();
     //========= PROCESAR =========
     ActualizarHitboxes();
+    DetectarColisiones();
+    GestionarColisiones();
+    AnimarEnemigo();
     //========= DIBUJAR =========
     Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.Beige);
+        DibujarTexto();
         DibujarObjetos();
         GestionarDebugMode();
     Raylib.EndDrawing();
@@ -259,6 +124,15 @@ Raylib.CloseWindow();
 #endregion
 
 #region ==== METODOS ====
+void ActualizarTemporizadores()
+{
+    // Obtener el tiempo en segundos para el último fotograma dibujado (tiempo delta)
+    deltaTime = Raylib.GetFrameTime();
+    // Sumamos tiempo trascurrido al disparo
+    tiempoEsperaDisparo += deltaTime;
+    // Se acumula tiempo para determinar cuando cambiar el frame
+    tiempoActualAnimacion += deltaTime;    
+}
 void MoverJugador()
 {
     if (activoJugador)
@@ -294,8 +168,7 @@ void DispararShuriken()
 }
 void MoverShuriken()
 {
-    
-   
+    if (activoShuriken) posicionShuriken.X += velocidadShuriken * deltaTime;
 }
 void ActualizarHitboxes()
 {
@@ -306,18 +179,126 @@ void ActualizarHitboxes()
     hitboxEnemigo.X = posicionEnemigo.X;
     hitboxEnemigo.Y = posicionEnemigo.Y;
 }
+void MoverEnemigo()
+{
+    if (activoEnemigo && !activoDisparo)
+    {
+        // Si el enemigo se mueve hacia arriba y no supera el limite superior 
+        if (upEnemigo && (posicionEnemigo.Y > limiteUp))
+        {
+            posicionEnemigo.Y -= velocidadEnemigo * deltaTime;
+        }
+        else
+        {
+            upEnemigo = false;
+        }
+        // Si el enemigo se mueve hacia abajo y no supera el limite inferior
+        if (!upEnemigo && (posicionEnemigo.Y < limiteDown))
+        {
+            posicionEnemigo.Y += velocidadEnemigo * deltaTime;
+        }
+        else
+        {
+            upEnemigo = true;
+        }
+    }
+}
+void DispararEnemigo()
+{
+    // Si tiempo espera disparo es mayo o igual que el refresco de disparo
+    if (activoEnemigo && (tiempoEsperaDisparo >= refrescoDisparo))
+    {
+        // Configuro el disparo como activado
+        activoDisparo = true;
+        // Reinicio el tiempo de espera para el siguiente disparo
+        tiempoEsperaDisparo = 0;
+        // Posicion Disparo en X segun Posicion Enemigo
+        posicionDisparo.X = posicionEnemigo.X - 50f;
+        // Posicion Disparo en Y segun Posicion Enemigo
+        posicionDisparo.Y = posicionEnemigo.Y + (spriteEnemigo.Height * 0.4f);
+    }
+}
+void AnimarEnemigo()
+{
+    if (activoEnemigo)
+    {
+        if (tiempoActualAnimacion >= tiempoEntreFrame)
+        {
+            //Reiniciar el conteo para el siguiente cuadro
+            tiempoActualAnimacion = 0f;
+            //Incrementar el INDICE del frame
+            frameActualAnimacion++;
+            //Si se supera el último frame
 
-void DetectarColisiones()
+            if (frameActualAnimacion >= cantidadFrames)
+            {
+                //Reiniciar la animacion
+                frameActualAnimacion = 0;
+            }
+            RectanguloAnimacion.X = RectanguloAnimacion.Width * columnaAnimacionY;
+            //Posicionar el rectángulo en el siguiente cuadro
+            RectanguloAnimacion.Y = frameActualAnimacion * RectanguloAnimacion.Height;
+        }
+    }
+}
+void MoverDisparoEnemigo()
 {
     
+    // Si el Disparo esta activo
+    if (activoDisparo)
+    {
+        // Mover Disparo horizontalmente en Sincronización delta
+        posicionDisparo.X -= velocidadDisparo * deltaTime;
+        // Detectamos colision entre hibtox Disparo y hibox Jugador
+        
+    }
+    // Si el Disparo supera limite izquierdo
+    if (posicionDisparo.X < 0)
+    {
+        activoDisparo = false;
+        posicionDisparo.X = posicionEnemigo.X;
+    }
 }
+void DetectarColisiones()
+{
+    collisionShurikenMapa = Raylib.CheckCollisionRecs(hitboxShuriken, hitboxPared);
+    collisionShurikenEnemigo = Raylib.CheckCollisionRecs(hitboxShuriken, hitboxEnemigo);
+    collisionDisparoJugador = Raylib.CheckCollisionCircleRec(posicionDisparo, radioDisparo, hitboxJugador);
+}
+void GestionarColisiones()
+{
+    if (collisionShurikenMapa)
+    {
+        activoShuriken = false;
+        posicionShuriken.X = origenShurikenX;
+    }
 
+    if (collisionShurikenEnemigo)
+    {
+        activoShuriken = false;
+        posicionShuriken.X = origenShurikenX;
+        activoEnemigo = false;
+    }
+    
+    if (collisionDisparoJugador)
+    {
+        activoJugador = false;
+        activoDisparo = false;
+        tiempoEsperaDisparo = 0;
+    }
+}
 void DibujarObjetos()
 {
-    if (activoJugador)  Raylib.DrawTextureV(spriteJugador, posicionJugador, Color.White);
-    if (activoEnemigo)  Raylib.DrawTextureRec(enemigoAnimamacionReposo, RectanguloAnimacion, posicionEnemigo, Color.Red);
+    if (activoJugador) Raylib.DrawTextureV(spriteJugador, posicionJugador, Color.White);
+    if (activoEnemigo) Raylib.DrawTextureRec(enemigoAnimamacionReposo, RectanguloAnimacion, posicionEnemigo, Color.Red);
     if (activoShuriken) Raylib.DrawTextureV(spriteShuriken, posicionShuriken, Color.White);
-    if (activoDisparo)  Raylib.DrawCircleV(posicionDisparo, radioDisparo, Color.Black);
+    if (activoDisparo) Raylib.DrawCircleV(posicionDisparo, radioDisparo, Color.Black);
+}
+void DibujarTexto()
+{
+    if (collisionDisparoJugador) Raylib.DrawText("GAME OVER", 50, 100, 75, Color.Red);
+    if (collisionShurikenEnemigo) Raylib.DrawText("VICTORIA", 50, 100, 75, Color.DarkGreen);
+    if (activoEnemigo) Raylib.DrawText("FRAME ENEMIGO " + frameActualAnimacion.ToString(), 10, 1000, 30, Color.DarkBrown);
 }
 void CargarTexturas()
 {
