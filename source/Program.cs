6109842ff5ae8e97bac7a583269fdced6f92185f
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-09 08:37:03 -0300

    Refactor: GestionDebugMode, DibujarHitboxs, CargaTexturas, DispararShuriken y MoverJugador

--- source/Program.cs
+++ source/Program.cs
@@ -2,14 +2,16 @@
 using Raylib_cs;
 using System.Numerics;
 // =========== Constantes ===== //
-const int SCREEN_WIDTH = 320;
-const int SCREEN_HEIGHT = 180;
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+const int ESCALA_ACTORES = 10;
+const int ESCALA_ITEMS = 6;
+#region ==== PROPIEDADES ====
 // =========== Jugador ===== //
 Vector2 posicionJugador = new Vector2(50, 400);
 Texture2D spriteJugador;
 Rectangle hitboxJugador;
 float velocidadJugador = 500f;
-int escalaJugador = 8;
 bool activoJugador = true;
 // =========== Shuriken ===== //
 Vector2 posicionShuriken = new Vector2(100, 400);
@@ -17,15 +19,15 @@ float velocidadShuriken = 1000f;
 Texture2D spriteShuriken;
 bool activoShuriken = false;
 Rectangle hitboxShuriken;
-int escalaShuriken = 4;
 // =========== Enemigo ===== //
 Vector2 posicionEnemigo ;
-float velocidadEnemigo = 300f;
+float velocidadEnemigo = 400f;
+int dificultad = 3;
+velocidadEnemigo = velocidadEnemigo * dificultad;
 Texture2D spriteEnemigo;
 bool activoEnemigo = true;
 bool upEnemigo = true;
 Rectangle hitboxEnemigo;
-int escalaEnemigo = 8;
 // =========== Animacion Enemigo Reposo ===== //
 float duracionAnimacion = 1.0f;
 float velocidadAnimacion = 6f;
@@ -49,31 +51,33 @@ int anchoPared = 10;
 bool collisionShurikenMapa = false;
 bool collisionShurikenEnemigo = false;
 bool collisionDisparoJugador = false;
-// Inicializar ventana
+#endregion
+
+bool isDebugMode = false;
+float deltaTime = 0f;
+
+#region ==== CARGA DE JUEGO ====
 Raylib.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bullet Hell con Raylib");
 //Configurar ventana sin border
-Raylib.ToggleBorderlessWindowed();
+//Raylib.ToggleBorderlessWindowed();
 // =========== Cargar Texturas ===== //
-spriteShuriken = Raylib.LoadTexture("sprites/Shuriken.png");
-spriteJugador = Raylib.LoadTexture("sprites/Jugador.png");
-spriteEnemigo = Raylib.LoadTexture("sprites/Enemigo.png");
-enemigoAnimamacionReposo = Raylib.LoadTexture("sprites/Enemigo/Idle.png");
+CargaTexturas();
 // =========== Redimensionar Texturas ===== //
-spriteShuriken.Width  *= escalaShuriken;
-spriteShuriken.Height *= escalaShuriken;
-spriteJugador.Width  *= escalaJugador;
-spriteJugador.Height *= escalaJugador;
-spriteEnemigo.Width  *= escalaEnemigo;
-spriteEnemigo.Height *= escalaEnemigo;
+spriteShuriken.Width  *= ESCALA_ITEMS;
+spriteShuriken.Height *= ESCALA_ITEMS;
+spriteJugador.Width  *= ESCALA_ACTORES;
+spriteJugador.Height *= ESCALA_ACTORES;
+spriteEnemigo.Width  *= ESCALA_ACTORES;
+spriteEnemigo.Height *= ESCALA_ACTORES;
 //Escalar Spritesheet
-enemigoAnimamacionReposo.Width *= escalaEnemigo;
-enemigoAnimamacionReposo.Height *= escalaEnemigo;
+enemigoAnimamacionReposo.Width *= ESCALA_ACTORES;
+enemigoAnimamacionReposo.Height *= ESCALA_ACTORES;
 //Definir rectángulo segun escala de spritesheet
 RectanguloAnimacion = new Rectangle(
                         0f,
                         0f,
                       enemigoAnimamacionReposo.Width / cantidadFrames,
-                      enemigoAnimamacionReposo.Height);
+                      enemigoAnimamacionReposo.Height/ cantidadFrames);
 //=========== Generar Posiciones ===== //
 posicionPared = new Vector2(Raylib.GetScreenWidth() - anchoPared, 0);
 posicionEnemigo = new Vector2(Raylib.GetScreenWidth() - (spriteEnemigo.Width * 2f), posicionJugador.Y);
@@ -90,55 +94,45 @@ float limiteDown = Raylib.GetScreenHeight() - spriteJugador.Height;
 // Calculo de origen proyectil segun ancho jugador
 float origenShurikenX = posicionJugador.X + spriteJugador.Width;
 // Hasta que la aplicacion no se cierre
+float tiempoEntreFrame = duracionAnimacion / velocidadAnimacion;
+int columnaAnimacionY = 0;
+#endregion
+#region ==== GAME LOOP ====
 while (!Raylib.WindowShouldClose())
 {
     // Obtener el tiempo en segundos para el último fotograma dibujado (tiempo delta)
-    float deltaTime = Raylib.GetFrameTime();
+    deltaTime = Raylib.GetFrameTime();
     // Sumamos tiempo trascurrido al disparo
     tiempoEsperaDisparo += deltaTime;
     // Se acumula tiempo para determinar cuando cambiar el frame
     tiempoActualAnimacion += deltaTime;
     // Si hay que cambiar de cuandro de animacion (segun duracion y velocidad)
-    if (tiempoActualAnimacion >= duracionAnimacion / velocidadAnimacion) {
+    //velocidadAnimacion++;
+    if (tiempoActualAnimacion >= tiempoEntreFrame)
+    {
         //Reiniciar el conteo para el siguiente cuadro
         tiempoActualAnimacion = 0f;
         //Incrementar el INDICE del frame
         frameActualAnimacion++;
         //Si se supera el último frame
+
         if (frameActualAnimacion >= cantidadFrames)
         {
             //Reiniciar la animacion
             frameActualAnimacion = 0;
-        } 
+        }
+        RectanguloAnimacion.X = RectanguloAnimacion.Width * columnaAnimacionY;
         //Posicionar el rectángulo en el siguiente cuadro
-        RectanguloAnimacion.X = frameActualAnimacion * RectanguloAnimacion.Width;
+        RectanguloAnimacion.Y = frameActualAnimacion * RectanguloAnimacion.Height;
     }
     Raylib.BeginDrawing();
     // Establecer el color de fondo
     Raylib.ClearBackground(Color.Beige);
     // Establecer mensaje para ver frame actual del enemigo
-    Raylib.DrawText("FRAME ENEMIGO "+ frameActualAnimacion.ToString(), 10, 1000, 30, Color.DarkBrown);
+    Raylib.DrawText("FRAME ENEMIGO " + frameActualAnimacion.ToString(), 10, 1000, 30, Color.DarkBrown);
     // Si el jugador presiona la tecla UP y no supera el limite superior
-    if (Raylib.IsKeyDown(KeyboardKey.Up) && (posicionJugador.Y > limiteUp))
-    {
-        // Mover Jugador hacia arriba en Sincronización delta
-        posicionJugador.Y -= velocidadJugador * deltaTime;
-    }
-    // Si el jugador presiona la tecla Down y no supera el limite inferior
-    if (Raylib.IsKeyDown(KeyboardKey.Down) && (posicionJugador.Y < limiteDown))
-    {
-        // Mover Jugador hacia abajo en Sincronización delta
-        posicionJugador.Y += velocidadJugador * deltaTime;
-    }
-    // Si el jugador presiona la tecla F y aún no disparó Shuriken
-    if (Raylib.IsKeyPressed(KeyboardKey.F) && !activoShuriken && activoJugador)
-    {
-        //Asignar a la shuriken la posicion de origen en X
-        posicionShuriken.X = origenShurikenX;
-        //Calcula de la posicion en Y luego de moverse y segun la altura del personaje
-        posicionShuriken.Y = posicionJugador.Y + (spriteJugador.Height * 0.4f);
-        activoShuriken = true;
-    }
+    MoverJugador();
+    DispararShuriken();
     // Si el Shuriken esta activo
     if (activoShuriken)
     {
@@ -158,7 +152,7 @@ while (!Raylib.WindowShouldClose())
         // Dibujar Textura en pantalla [textura2D, vector2, color]
         Raylib.DrawTextureV(spriteShuriken, posicionShuriken, Color.White);
         // Dibujar Hibox en pantalla [Rectangulo, color] (Modo Debug)
-        Raylib.DrawRectangleRec(hitboxShuriken, Raylib.ColorAlpha(Color.Red, 0.3f));
+
     }
     // Si el Jugador esta activo
     if (activoJugador)
@@ -169,7 +163,7 @@ while (!Raylib.WindowShouldClose())
         // Dibujar Textura en pantalla [textura2D, vector2, color]
         Raylib.DrawTextureV(spriteJugador, posicionJugador, Color.White);
         // Dibujar Hibox en pantalla [Rectangulo, color] (Modo Debug)
-        Raylib.DrawRectangleRec(hitboxJugador, Raylib.ColorAlpha(Color.Blue, 0.3f));
+
     }
     else
     {
@@ -211,9 +205,9 @@ while (!Raylib.WindowShouldClose())
             activoEnemigo = false;
         }
         // Dibujar una porción de la textura usando un rectángulo
-        Raylib.DrawTextureRec(enemigoAnimamacionReposo, RectanguloAnimacion, posicionEnemigo, Color.White);
-        // Dibujar Hibox en pantalla [Rectangulo, color] (Modo Debug)
-        Raylib.DrawRectangleRec(hitboxEnemigo, Raylib.ColorAlpha(Color.Black, 0.3f));
+        //Raylib.DrawTextureV(spriteEnemigo, posicionEnemigo, Color.DarkBlue);
+        Raylib.DrawTextureRec(enemigoAnimamacionReposo, RectanguloAnimacion, posicionEnemigo, Color.Red);
+    
     }
     else
     {
@@ -231,6 +225,8 @@ while (!Raylib.WindowShouldClose())
         posicionDisparo.X = posicionEnemigo.X;
         // Posicion Disparo en Y segun Posicion Enemigo
         posicionDisparo.Y = posicionEnemigo.Y + (spriteEnemigo.Height * 0.4f);
+        //
+        columnaAnimacionY = 2;
     }
     // Si el Disparo esta activo
     if (activoDisparo)
@@ -254,10 +250,88 @@ while (!Raylib.WindowShouldClose())
         activoDisparo = false;
         posicionDisparo.X = posicionEnemigo.X;
     }
-    // Dibujar Pantalla en pantalla [Rectangulo, color] (Modo Debug)
-    Raylib.DrawRectangleRec(hitboxPared, Color.Brown);
-    // Finalizar el dibujo del Canvas 
+
+    GestionDebugMode();
     Raylib.EndDrawing();
 }
 // Cerrar ventana
 Raylib.CloseWindow();
+#endregion
+
+#region ==== METODOS ====
+void MoverJugador()
+{
+    if (activoJugador)
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.Up) && (posicionJugador.Y > limiteUp))
+        {
+            // Mover Jugador hacia arriba en Sincronización delta
+            posicionJugador.Y -= velocidadJugador * deltaTime;
+        }
+        // Si el jugador presiona la tecla Down y no supera el limite inferior
+        if (Raylib.IsKeyDown(KeyboardKey.Down) && (posicionJugador.Y < limiteDown))
+        {
+            // Mover Jugador hacia abajo en Sincronización delta
+            posicionJugador.Y += velocidadJugador * deltaTime;
+        }
+    }
+
+    
+}
+void DispararShuriken()
+{
+    if (!activoJugador) return;
+
+    // Si el jugador presiona la tecla F y aún no disparó Shuriken
+    if (Raylib.IsKeyPressed(KeyboardKey.F) && !activoShuriken)
+    {
+        //Asignar a la shuriken la posicion de origen en X
+        posicionShuriken.X = origenShurikenX;
+        //Calcula de la posicion en Y luego de moverse y segun la altura del personaje
+        posicionShuriken.Y = posicionJugador.Y + (spriteJugador.Height * 0.4f);
+        activoShuriken = true;
+    }
+}
+void MoverShuriken()
+{
+
+}
+void ActulizarHitboxes()
+{
+    
+}
+void CargaTexturas()
+{
+    spriteShuriken = Raylib.LoadTexture("sprites/Shuriken.png");
+    spriteJugador = Raylib.LoadTexture("sprites/Jugador.png");
+    spriteEnemigo = Raylib.LoadTexture("sprites/Enemigo.png");
+    enemigoAnimamacionReposo = Raylib.LoadTexture("sprites/Enemigo/Walk.png");
+}
+void DibujarHitboxs()
+{
+    // Dibujar Pantalla en pantalla [Rectangulo, color] (Modo Debug)
+    Raylib.DrawRectangleRec(hitboxPared, Color.Brown);
+    Raylib.DrawRectangleRec(hitboxShuriken, Raylib.ColorAlpha(Color.Red, 0.3f));
+    Raylib.DrawRectangleRec(hitboxJugador, Raylib.ColorAlpha(Color.Blue, 0.3f));
+    // Dibujar Hibox en pantalla [Rectangulo, color] (Modo Debug)
+    Raylib.DrawRectangleRec(hitboxEnemigo, Raylib.ColorAlpha(Color.Black, 0.3f));
+}
+void GestionDebugMode()
+{
+    // Finalizar el dibujo del Canvas 
+    if (Raylib.IsKeyDown(KeyboardKey.F12))
+    {
+        isDebugMode = true;
+    }
+
+    if (Raylib.IsKeyDown(KeyboardKey.F11))
+    {
+        isDebugMode = false;
+    }
+
+    if (isDebugMode)
+    {
+        DibujarHitboxs();
+    }
+}
+#endregion
